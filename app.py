a2a5e3223ff7b04f20455417a5728c4cc7f57e6b
FumaGuma <jasam42@hotmail.com>
2022-01-17 20:15:33 +0100

    new version

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
 from markupsafe import escape
-import imageproc as ip
+import imageprocessing as ip
 import os
-import equation_solver as es
-
-PEOPLE_FOLDER = os.path.join('static', 'people_photo')
+import solver
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = PEOPLE_FOLDER
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
@@ -17,35 +14,28 @@ def show_index():
     if request.method == 'POST':
         file = request.files['file']
         file.save(os.path.join(os.path.abspath(os.getcwd()),'static/upload_brojevi.jpg'))
-        return redirect(url_for('show_segmented'))
-    return render_template("form.html")
+        return redirect(url_for('show_equation'))
+    return render_template("index.html")
 
-@app.route('/seg',methods=('GET','POST'))
-def show_segmented():
+@app.route('/equation',methods=('GET','POST'))
+def show_equation():
     solution=''
     if request.method == 'POST':
         equation = request.form['content']
-        solution = es.solve(equation)
+        return redirect(url_for('show_results',equation=equation))
     full_filename = 'static/upload_brojevi.jpg'#os.path.join('static','upload_brojevi.jpg')
-    rect = ip.get_rectangles(full_filename)
-    ip.save_img_with_rect(full_filename,rect)
-    brojevi = 'static/upload_brojevi_rect.jpg'
-    crop = ip.cropped_dataset(full_filename,rect)
-    eq = ip.data_to_model(crop)
-    return render_template("index.html", image = brojevi, string = eq, solution = solution)
+    rect_image = ip.save_img_with_rect(full_filename)
+    eq = ip.process_image(full_filename)
+    return render_template("equation.html", image = rect_image, string = eq)
 
-@app.route("/test")
-def hello_world():
-    return render_template("index.html", image = 'static/brojevi5.jpg')
-
-@app.route("/<name>")
-def hello(name):
-    return f"Hello, {escape(name)}!"
-
-@app.route("/upload", methods=('GET','POST'))
-def hello_test():
+@app.route('/results',methods=('GET','POST'))
+def show_results():
     if request.method == 'POST':
-        file = request.files['file']
-        file.save(os.path.join(os.path.abspath(os.getcwd()),'testbrojevi.jpg'))
         return redirect(url_for('show_index'))
-    return render_template("index.html")
+    equation = request.args.get('equation')
+    solution = solver.solve(equation)
+    if solution != "Invalid Expression":
+        solution = str(equation) + '=' + str(solution)
+    else:
+        solution = "Invalid Expression"
+    return render_template("results.html",string=solution)
